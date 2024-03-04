0c4c341f95d5d31d234399f37ef9154e98e357bf
Roben Kleene <contact@robenkleene.com>
2024-03-04 06:03:39 -0500

    Convert scale to function

--- javascript/middleware_scales.js
+++ javascript/middleware_scales.js
@@ -1,41 +1,8 @@
-// Re-compile the file automatically when it changes
-// This is probably pointless because this script stores state in `var values`
-// autowatch = 1;
-
-// Inlets & Outlets
-INTERVALS = 0;
-STEP_COUNT = 1;
-BASE_NOTE = 2;
-PITCH_MIN = 3;
-PITCH_MAX = 4;
-inlets = 5;
-outlets = 1;
-
 OCTAVE_SIZE = 12;
 
-// Store input
-var values = [[], 0, 0, 0, 0];
-
-function msg_int(value) {
-  values[inlet] = value;
-}
-
-function list() {
-  var arr = arrayfromargs(arguments);
-  values[inlet] = arr.map(Number);
-}
-
-function bang() {
-  trigger_output();
-}
-
-function trigger_output() {
-  var intervals = values[INTERVALS];
-  var stepCount = values[STEP_COUNT];
-  var min = values[PITCH_MIN];
-  var max = values[PITCH_MAX];
-  var base = values[BASE_NOTE];
-  note = base;
+var scale;
+scale = function scale(intervals, stepCount, base, min, max) {
+  var note = base;
   if (note < min) {
     note = min;
   }
@@ -60,9 +27,5 @@ function trigger_output() {
     }
     notes.push(note);
   }
-  outlet(0, notes.join(' '));
-}
-
-function log(obj) {
-  post(JSON.stringify(obj));
-}
+  return notes.join(' ');
+};
