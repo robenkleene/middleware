8bb116b6df64f35e775359bceffaae5f3b9bf7c0
Roben Kleene <contact@robenkleene.com>
2024-03-25 07:23:02 -0400

    WIP Work on distribute

--- javascript/middleware_distribute.js
+++ javascript/middleware_distribute.js
@@ -1,10 +1,16 @@
 var distribute;
 distribute = function(euclidean, division, scale, durations) {
+  notes = [];
   startTime = 0
   for (var i = 0; i < euclidean.length; i++) {
     var pulse = euclidean[i];
     if (pulse > 0) {
-    } else {
+      var note = {
+        start_time: startTime,
+        pitch: scale[i % scale.length],
+        duration: durations[i % durations.length]
+      };
     }
+    startTime += division;
   }
 }
