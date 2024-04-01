119c453941b27a523e008b7b448669c92e79b190
Roben Kleene <contact@robenkleene.com>
2024-04-01 15:19:05 -0400

    Work on updating script for dials

--- javascript/middleware_generator.js
+++ javascript/middleware_generator.js
@@ -11,30 +11,38 @@ include("middleware_intervals");
 include("middleware_scales");
 include("middleware_distribute");
 
-BASE_NOTE = 60;
-MIN_NOTE = 0;
-MAX_NOTE = 127;
+
+var baseNote = 60;
+var minNote = 1;
+var maxNote = 128;
+var division = 0.25;
+var duration = division;
+var steps = 8;
+var pulses = steps;
+var velocity = 127;
 
 function bang() {
-	var division = 0.25;
-	var duration = division;
 	var intervals = getIntervals("minor");
-	var scale = generateScale(intervals, intervals.length + 1, BASE_NOTE, MIN_NOTE, MAX_NOTE);
-	var steps = intervals.length;
-	var pulses = Math.round(intervals.length * 1.5);
+	var scale = generateScale(intervals, intervals.length + 1, baseNote, minNote, maxNote);
 	var durations = Array(steps);
 	for(var i = 0; i < durations.length; i++) {
 		durations[i] = duration;
 	}
+	var velocities = Array(steps);
+	for(var i = 0; i < velocities.length; i++) {
+		velocities[i] = velocity;
+	}
 	var euclidean = generateEuclidean(steps, pulses, durations);
-	var notesArr = distribute(euclidean, division, scale, durations);
-	// Strangely `d.setparse("notes", notesArr);` doesn't work?
+	var notesArr = distribute(euclidean, division, scale, durations, velocities);
 	var notes = { notes: notesArr };
 	var notesJSON = JSON.stringify(notes);
 	var d = new Dict("output");;
 	d.parse(notesJSON);
 	output(notesJSON);
 	output(d.stringify());
+}
+
+function list() {
 }
 
 function output(text) {
