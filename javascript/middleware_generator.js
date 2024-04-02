8bcc0dbae6a8e3f90b3cced70f4af843a202d4e3
Roben Kleene <contact@robenkleene.com>
2024-04-02 09:21:28 -0400

    Tweak script state

--- javascript/middleware_generator.js
+++ javascript/middleware_generator.js
@@ -23,22 +23,29 @@ var minNoteDial = tabs.getnamed('dial4');
 var maxNoteDial = tabs.getnamed('dial5');
 var velocityDial = tabs.getnamed('dial6');
 
+baseNoteDial.message('set', 60);
+maxNoteDial.message('set', 127);
+minNoteDial.message('set', 1);
+pulsesDial.message('set', 8);
+stepsDial.message('set', 8);
+velocityDial.message('set', 127);
+
 function bang() {
-	var pulses = pulsesDial.getvalueof();
-	var steps = stepsDial.getvalueof();
-	var baseNote = baseNoteDial.getvalueof();
-	var minNote = minNoteDial.getvalueof();
-	var maxNote = maxNoteDial.getvalueof();
-	var velocity = velocityDial.getvalueof();
+  var pulses = pulsesDial.getvalueof();
+  var steps = stepsDial.getvalueof();
+  var baseNote = baseNoteDial.getvalueof();
+  var minNote = minNoteDial.getvalueof();
+  var maxNote = maxNoteDial.getvalueof();
+  var velocity = velocityDial.getvalueof();
 	var velocities = Array(pulses);
-	for(var i = 0; i < velocities.length; i++) {
+	for (var i = 0; i < velocities.length; i++) {
 		velocities[i] = velocity;
 	}
 
 	var division = c.get("grid::interval");
 	var duration = division;
 	var durations = Array(pulses);
-	for(var i = 0; i < durations.length; i++) {
+	for (var i = 0; i < durations.length; i++) {
 		durations[i] = duration;
 	}
 
@@ -51,15 +58,6 @@ function bang() {
 	d.parse(notesJSON);
 	output(notesJSON);
 	output(c.stringify());
-}
-
-function loadbang() {
-	baseNoteDial.message('set', 60);
-	maxNoteDial.message('set', 127);
-	minNoteDial.message('set', 1);
-	pulsesDial.message('set', 8);
-	stepsDial.message('set', 8);
-	velocityDial.message('set', 127);
 }
 
 function output(text) {
