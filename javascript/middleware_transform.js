23e2cdaa262efb645652b1f43876f1a0dc758272
Roben Kleene <contact@robenkleene.com>
2024-04-05 19:16:00 -0400

    Work on transform

---
+++ javascript/middleware_transform.js
@@ -0,0 +1,20 @@
+var transform;
+transform = function(euclidean, division, notes, durations, min, max) {
+	var notes = [];
+	startTime = 0
+	for (var i = 0; i < euclidean.length; i++) {
+		var pulse = euclidean[i];
+		if (pulse > 0) {
+			var note = {
+				start_time: startTime,
+				pitch: scale[i % scale.length],
+				duration: durations[i % durations.length],
+				velocity: velocities[i % velocities.length]
+			};
+			notes.push(note);
+		}
+		startTime += division;
+	}
+	return notes;
+}
+
